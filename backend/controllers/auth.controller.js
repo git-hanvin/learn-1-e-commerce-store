cf68ebf75fe4106d9adf0f71079bc1d2534033b5
git-hanvin <hanvinaditya155@gmail.com>
2024-10-29 22:03:03 +0700

    signup, logout, login is done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,11 +2,10 @@ import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
-// 
 const generateToken = (userId) => {
     const accessToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "15m"});
 
-    const refreshToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "7d"});
+    const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: "7d"});
 
     return { accessToken, refreshToken }
 };
@@ -19,16 +18,18 @@ const setCookies = (res, accessToken, refreshToken) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true, // prevent XSS attacks, cross-site scripting attack
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict", // prevent CSRF attacks, cross-site request forgery
+        sameSite: "none", // prevent CSRF attacks, cross-site request forgery
         maxAge: 15 * 60 * 1000 // 15 minutes
     });
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true, // prevent XSS attacks, cross-site scripting attack
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict", // prevent CSRF attacks, cross-site request forgery
+        sameSite: "none", // prevent CSRF attacks, cross-site request forgery
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 }
+
+// SIGN IN FUNCTION
 export const signup = async (req, res) =>{
     // desktop app => postman
     const { email,password,name } = req.body
@@ -47,23 +48,69 @@ export const signup = async (req, res) =>{
 
         setCookies(res, accessToken, refreshToken);
 
-        res.status(201).json({ user:{
+        res.status(201).json({ 
             _id: user._id,
             name: user.name,
             email: user.email,
             role: user.role
-        }, 
-        message: "User created successfully"});
+        });
     } catch (error) {
+        console.log("Error in signup controller", error.message);
         res.status(500).json({message: error.message});
     }
     // res.send("Sign up route called"); // only for testing
 };
 
 export const login = async (req, res) =>{
-    res.send("login route called");
+    try {
+        const {email,password} = req.body
+        const user = await User.findOne({email});
+
+        if (user && (await user.comparePassword(password))) {
+            const { accessToken, refreshToken } = generateToken(user._id);
+
+            await storeRefreshToken(user._id, refreshToken);
+            setCookies(res, accessToken, refreshToken);
+
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            });
+        } else {
+            return res.status(401).json({message: "Invalid email or password"});
+        }
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({message: error.message});
+    }
+    
+    
+    
+    
+    // res.send("login route called");
 };
 
-export const logout = async (req, res) =>{
-    res.send("logout route called");
+// LOGOUT FUNCTION
+// the explain of code = if the user want to logout we get the refresh token from cookie and delete it from redis
+export const logout = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken; // we need to add middleware to get refresh token from cookie (on file server.js)
+        console.log("refreshToken from cookies:", refreshToken); // Log the refresh token
+        
+        if(refreshToken){
+            const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET); // Verification should now match with generation
+            // console.log("Decoded token:", decoded); // Log the decoded token
+            await redis.del(`refresh_token:${decoded.userId}`);
+        }
+
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        res.json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.error("Error in logout controller", error.message);
+        res.status(500).json({message: "Server error", error: error.message});
+    }
+    // res.send("logout route called");
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // const express = require("express"); // ini adalah syntax lama
 import express from "express"; // for const app and func app.listen | this syntax can be use because u use type: module on package.json
 import dotenv from "dotenv"; // for running the PORT that connect to .env
+import cookieParser from "cookie-parser"; // for auth.controller so we can req the refresh cookie
 
 // routes
 import authRoutes from "./routes/auth.route.js";
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 // console.log(process.env.PORT);
 
 app.use(express.json()); // this allows you to parse body of the request (POSTMAN)
+app.use(cookieParser());
 
 // authentification
 app.use("/api/auth", authRoutes); // bacanya ketika kita masukan localhost:5000/api/auth, /signup akan pindah kesana (auth.route.js)
