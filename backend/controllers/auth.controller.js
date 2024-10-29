ddfdad9dca38e40ad210b228044e3db3b3352100
git-hanvin <hanvinaditya155@gmail.com>
2024-10-29 19:59:05 +0700

    done authentification and signin already done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
+// 
 const generateToken = (userId) => {
     const accessToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "15m"});
 
@@ -14,13 +15,27 @@ const storeRefreshToken = async (userId, refreshToken) => {
     await redis.set(`refresh_token:${userId}`, refreshToken,"EX",7*24*60*60); // 7 days
 }
 
+const setCookies = (res, accessToken, refreshToken) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true, // prevent XSS attacks, cross-site scripting attack
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", // prevent CSRF attacks, cross-site request forgery
+        maxAge: 15 * 60 * 1000 // 15 minutes
+    });
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true, // prevent XSS attacks, cross-site scripting attack
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", // prevent CSRF attacks, cross-site request forgery
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    });
+}
 export const signup = async (req, res) =>{
     // desktop app => postman
     const { email,password,name } = req.body
+    // this func is checking if theres a user with the same email or not, if not it will create a new user
     try {
         const userExists = await User.findOne({ email });
 
-        // if users exists
         if(userExists) {
             return res.status(400).json({message: "User already exists"});
         }
@@ -30,7 +45,15 @@ export const signup = async (req, res) =>{
         const { accessToken, refreshToken } = generateToken(user._id);
         await storeRefreshToken(user._id, refreshToken);
 
-        res.status(201).json({user, message: "User created successfully"});
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(201).json({ user:{
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+        }, 
+        message: "User created successfully"});
     } catch (error) {
         res.status(500).json({message: error.message});
     }
